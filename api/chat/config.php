01da970e65432a19d330b5d92119288bcde8e164
hananchaliq <hananxpplg@gmail.com>
2026-06-15 21:07:25 +0800

    remove the old api with php

--- api/chat/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// config.php
-define('GEMINI_API_KEY', 'AIzaSyBQ7gRljQfq7x-AWBc0406IMbjuc7WPCWs');
-define('GEMINI_MODEL', 'gemini-2.0-flash-lite');
-
-// Konteks Admin Masjid Ar-Razzaq
-$system_context = "Kamu adalah admin resmi Masjid Ar-Razzaq. Jawab dengan ramah, sopan, dan religius.";
